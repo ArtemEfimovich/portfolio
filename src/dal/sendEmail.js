23b0fd1522a9959b3374c2a046060c23d2997972
ArtemEfimovich <artemfender@gmail.com>
2022-12-17 00:39:01 +0300

    fix post request

--- src/dal/sendEmail.js
+++ src/dal/sendEmail.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 export const sendEmail = async (data) => {
-    await axios.post('https://gmail-smtp-ah1v.vercel.app/sendMessage', {data})
+    await axios.post('https://gmail-smtp-five.vercel.app/sendMessage', {data})
         .then(res => {
             console.log('Thanks')
         })
